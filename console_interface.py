531884142748f0fccc668a4698aa317de25ec17e
Khilya Dmitry <demonukraine96@gmail.com>
2017-08-26 00:41:07 +0300

    Added max_balance functionality for console interface

--- console_interface.py
+++ console_interface.py
@@ -12,18 +12,21 @@ parser.add_argument('-b', '--balance', action='store_true', help='Will get and s
 parser.add_argument('-hi', '--history', action='store_true', help='Will get and show history of transactions')
 parser.add_argument('-hc', '--cache_history', action='store_true', help='Will cache history of transactions')
 parser.add_argument('-re', '--requests_empty', help='Will stop doing requests if got N empty transactions in a row')
+parser.add_argument('-mb', '--max_balance', action='store_true', help='Will show day when was card max ballance')
 
 def main(args):
     api = Privat24API(args.merchant_id, args.password)
     if args.balance:
         print('Current balance:', api.card_balance(args.card_number))
-    if args.history or args.cache_history:
+    if args.history or args.cache_history or args.max_balance:
         history = list(api.history(args.card_number, from_date=args.from_date, to_date=args.to_date, stop_empty_requests=args.requests_empty))
     if args.history:
         for item in history:
             print('[{} {trantime}][{rest}] {cardamount} {description} {terminal}'.format('{}.{}.{}'.format(*item['trandate'].split('-')[::-1]), **item), flush=True)
 
-    # print(max([(float(x['rest'].strip(' UAH')), x) for x in history], key=lambda x: x[0]))
+    if args.max_balance:
+        max_ballance = max([(float(x['rest'].strip(' UAH')), x) for x in history], key=lambda x: x[0])
+        print('Max card balance {:,} was {}'.format(max_ballance[0], max_ballance[1]['trandate']))
 
 if __name__ == '__main__':
     main(parser.parse_args())
