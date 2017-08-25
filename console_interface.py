cdc88ca01b97f9caab0de2e20c9d3951c8674760
Khilya Dmitry <demonukraine96@gmail.com>
2017-08-26 01:39:20 +0300

    Added income per month report functionality to console interface

--- console_interface.py
+++ console_interface.py
@@ -1,4 +1,6 @@
 import argparse
+import datetime
+from dateutil.relativedelta import relativedelta
 
 from core import Privat24API
 
@@ -16,22 +18,66 @@ parser.add_argument('-hc', '--cache_history', action='store_true', help='Will ca
 parser.add_argument('-re', '--requests_empty', help='Will stop doing requests if got N empty transactions in a row')
 parser.add_argument('-hp', '--hide_progress', action='store_true', help='Will hide history receiving progress')
 
-
 parser.add_argument('-b', '--balance', action='store_true', help='Will get and show ballance of card')
 parser.add_argument('-mb', '--max_balance', action='store_true', help='Will show day when was card max ballance')
 
+parser.add_argument('-rm', '--report_per_mounth', action='store_true', help='Will shop card income per month')
+parser.add_argument('-rmd', '--report_per_month_range', help='Will calculate average considering +N mounth before and after', default=0)
+
 def main(args):
     api = Privat24API(args.merchant_id, args.password)
-    if args.balance:
-        print('Current balance:', api.card_balance(args.card_number))
-    if args.history or args.cache_history or args.max_balance:
+    if args.history or args.cache_history or args.max_balance or args.report_per_mounth:
         history = list(api.history(args.card_number, from_date=args.from_date, to_date=args.to_date, stop_empty_requests=args.requests_empty, show_progress=not args.hide_progress))
     if args.history:
         for item in history:
             print('[{} {trantime}][{rest}] {cardamount} {description} {terminal}'.format('{}.{}.{}'.format(*item['trandate'].split('-')[::-1]), **item), flush=True)
 
+
+    if args.report_per_mounth:
+        month_to_income = {}
+        for item in history:
+            if item['cardamount'].startswith('-'):
+                continue
+
+            date = datetime.datetime.strptime(item['trandate'], '%Y-%m-%d')
+            key = date.strftime('%Y-%m')
+            value = float(item['cardamount'].split(' ')[0])
+
+            if key not in month_to_income:
+                month_to_income[key] = 0
+            month_to_income[key] += value
+
+        if args.report_per_month_range:
+            buffer_month_to_income = {}
+            max_key = max([datetime.datetime.strptime(x, '%Y-%m') for x in month_to_income]).strftime('%Y-%m')
+            min_key = min([datetime.datetime.strptime(x, '%Y-%m') for x in month_to_income]).strftime('%Y-%m')
+            for key, value in month_to_income.items():
+                date = datetime.datetime.strptime(key, '%Y-%m')
+
+                needed_values = [value]
+                for i in range(1, int(args.report_per_month_range)+1):
+                    for side in (1, -1):
+                        current_key = (date + relativedelta(months=1*side)).strftime('%Y-%m')
+                        if current_key in month_to_income:
+                            needed_values.append(month_to_income[current_key])
+
+                used_months = 1
+                if key != max_key:
+                    used_months += int(args.report_per_month_range)
+                if key != min_key:
+                    used_months += int(args.report_per_month_range)
+
+                buffer_month_to_income[key] = sum(needed_values)/used_months
+            month_to_income = buffer_month_to_income
+
+        for key, value in month_to_income.items():
+            print('{date} - {value:,.2f}'.format(date=key, value=value))
+
+    if args.balance:
+        print('Current balance: {:,}'.format(api.card_balance(args.card_number)))
+
     if args.max_balance:
-        max_ballance = max([(float(x['rest'].strip(' UAH')), x) for x in history], key=lambda x: x[0])
+        max_ballance = max([(float(x['rest'].split(' ')[0]), x) for x in history], key=lambda x: x[0])
         print('Max card balance {:,} was {}'.format(max_ballance[0], max_ballance[1]['trandate']))
 
 if __name__ == '__main__':
