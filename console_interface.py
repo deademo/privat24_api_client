4c90a956174caef455271473bf6e219dba9844d7
Khilya Dmitry <demonukraine96@gmail.com>
2017-08-26 00:16:49 +0300

    Implemented from_date and to_date params for console interface.

--- console_interface.py
+++ console_interface.py
@@ -6,14 +6,20 @@ parser = argparse.ArgumentParser(description='Interface for privat24 api')
 parser.add_argument('-m', '--merchant_id', required=True, help='Merchant id from privat24')
 parser.add_argument('-p', '--password', required=True, help='Password from privat24')
 parser.add_argument('-c', '--card_number', required=True, help='Number of needed card')
+parser.add_argument('-f', '--from_date', help='Get history from date, by default today minus 5 years')
+parser.add_argument('-t', '--to_date', help='Get history to date, by default today')
+parser.add_argument('-b', '--balance', action='store_true', help='Will get and show ballance of card')
+parser.add_argument('-hi', '--history', action='store_true', help='Will get and show ballance of card')
 
 
 def main(args):
     api = Privat24API(args.merchant_id, args.password)
-    print(api.card_balance(args.card_number))
-    history = api.history(args.card_number)
-    for item in history:
-        print('[{} {trantime}][{rest}] {cardamount} {description} {terminal}'.format('{}.{}.{}'.format(*item['trandate'].split('-')[::-1]), **item), flush=True)
+    if args.balance:
+        print('Current balance:', api.card_balance(args.card_number))
+    if args.history:
+        history = list(api.history(args.card_number, from_date=args.from_date, to_date=args.to_date))
+        for item in history:
+            print('[{} {trantime}][{rest}] {cardamount} {description} {terminal}'.format('{}.{}.{}'.format(*item['trandate'].split('-')[::-1]), **item), flush=True)
 
     # print(max([(float(x['rest'].strip(' UAH')), x) for x in history], key=lambda x: x[0]))
 
